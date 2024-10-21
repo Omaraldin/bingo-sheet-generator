4b5da066766bf23a58f1fa1fc57234eac8d9ad9a
Omaraldin <=>
2024-10-21 21:18:26 +0300

    initee

--- start.py
+++ start.py
@@ -1,16 +1,17 @@
 from merge import merge_a5_to_a4
 from fill import generate_bingo_cards
 import os
- 
+import sys
 
 input_folder = "./out" 
 output_folder = "./sheets" 
 
 if __name__ == '__main__':
-    sheets_number = 50
+    print(sys.argv)
+    # sheets_number = sys.argv[]
     
-    os.makedirs(input_folder, exist_ok=True)
-    os.makedirs(output_folder, exist_ok=True)
+    # os.makedirs(input_folder, exist_ok=True)
+    # os.makedirs(output_folder, exist_ok=True)
 
-    generate_bingo_cards(sheets_number * 2)
-    merge_a5_to_a4(input_folder, output_folder)
+    # generate_bingo_cards(sheets_number * 2)
+    # merge_a5_to_a4(input_folder, output_folder)
